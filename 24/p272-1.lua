b64dd9e57f0802d92d2ebb83661a16d4facc6449
Yuhui Huang <mrhuangyuhui@gmail.com>
2022-01-10 03:09:48 +0000

    上传代码

---
+++ 24/p272-1.lua
@@ -0,0 +1,10 @@
+co =
+    coroutine.create(
+    function()
+        print("hi")
+    end
+)
+print(type(co)) --> thread
+print(coroutine.status(co)) --> suspended
+coroutine.resume(co) --> hi
+print(coroutine.status(co)) --> dead
