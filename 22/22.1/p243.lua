c9fdea0689555a1c9b1272ca84360518b346f7e0
Yuhui Huang <mrhuangyuhui@gmail.com>
2022-01-24 08:35:17 +0000

    上传代码

---
+++ 22/22.1/p243.lua
@@ -0,0 +1,13 @@
+function getfield(f)
+    local v = _G -- 从全局表开始
+    for w in string.gmatch(f, "[%a_][%w_]*") do
+        v = v[w]
+    end
+    return v
+end
+
+print(getfield("io.read"))
+
+--[[
+function: 0x557e2b96d1f0
+]]
