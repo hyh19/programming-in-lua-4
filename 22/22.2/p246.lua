e81a7d5c4df560e25e63d226f599a017a365eb42
Yuhui Huang <mrhuangyuhui@gmail.com>
2022-01-24 09:17:42 +0000

    上传代码

---
+++ 22/22.2/p246.lua
@@ -0,0 +1,23 @@
+local declaredNames = {}
+setmetatable(
+    _G,
+    {
+        __newindex = function(t, n, v)
+            if not declaredNames[n] then
+                local w = debug.getinfo(2, "S").what
+                if w ~= "main" and w ~= "C" then
+                    error("attempt to write to undeclared variable " .. n, 2)
+                end
+                declaredNames[n] = true
+            end
+            rawset(t, n, v) -- 进行真正的赋值
+        end,
+        __index = function(_, n)
+            if not declaredNames[n] then
+                error("attempt to read undeclared variable " .. n, 2)
+            else
+                return nil
+            end
+        end
+    }
+)
