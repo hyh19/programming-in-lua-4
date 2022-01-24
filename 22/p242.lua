5694059c7d76a6e308889c3c2e2481a9001e798a
Yuhui Huang <mrhuangyuhui@gmail.com>
2022-01-24 05:03:14 +0000

    上传代码

---
+++ 22/p242.lua
@@ -0,0 +1,43 @@
+for n in pairs(_G) do
+    print(n)
+end
+
+--[[
+$ lua p242.lua
+print
+bit32
+math
+pcall
+select
+next
+rawequal
+ipairs
+_G
+tonumber
+rawget
+rawset
+require
+dofile
+collectgarbage
+coroutine
+_VERSION
+utf8
+error
+rawlen
+loadfile
+arg
+assert
+table
+io
+load
+string
+type
+getmetatable
+xpcall
+tostring
+debug
+pairs
+os
+package
+setmetatable
+]]
