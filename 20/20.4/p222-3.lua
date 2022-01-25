8e45046b888d3d8233f4aaf3ba693bdfbe7f2c85
Yuhui Huang <mrhuangyuhui@gmail.com>
2022-01-25 02:50:41 +0000

    上传代码

---
+++ 20/20.4/p222-3.lua
@@ -0,0 +1,16 @@
+local key = {} -- 唯一的键
+local mt = {
+    __index = function(t)
+        return t[key]
+    end
+}
+function setDefault(t, d)
+    t[key] = d
+    setmetatable(t, mt)
+end
+
+tab = {x = 10, y = 20}
+print(tab.x, tab.z) --> 10      nil
+
+setDefault(tab, 0)
+print(tab.x, tab.z) --> 10      0
