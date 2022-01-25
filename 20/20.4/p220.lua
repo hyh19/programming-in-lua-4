4042bdd9857fc09e7c9ae7a7810fc76fd8a2547e
Yuhui Huang <mrhuangyuhui@gmail.com>
2022-01-25 04:41:11 +0000

    上传代码

---
+++ 20/20.4/p220.lua
@@ -0,0 +1,16 @@
+-- 创建具有默认值的原型
+prototype = {x = 0, y = 8, width = 100, height = 100}
+
+local mt = {} -- 创建一个元表
+
+function new(o) -- 声明构造函数
+    setmetatable(o, mt)
+    return o
+end
+
+mt.__index = function(_, key)
+    return prototype[key]
+end
+
+w = new {x = 10, y = 20}
+print(w.width) --> 100
