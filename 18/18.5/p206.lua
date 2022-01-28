a29fd59e550381acec200ccae8d33d927be36a04
Yuhui Huang <mrhuangyuhui@gmail.com>
2022-01-28 09:40:16 +0000

    上传代码

---
+++ 18/18.5/p206.lua
@@ -0,0 +1,27 @@
+file = io.open("test.txt", "r")
+io.input(file)
+
+function allwords(f)
+    for line in io.lines() do
+        for word in string.gmatch(line, "%w+") do
+            f(word) -- 调用函数
+        end
+    end
+end
+
+allwords(print)
+
+-- 重新回到文件开头
+file:seek("set")
+
+local count = 0
+
+allwords(
+    function(w)
+        if w == "hello" then
+            count = count + 1
+        end
+    end
+)
+
+print(count)
