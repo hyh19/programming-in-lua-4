951df656bb21c999e64e81873766d941657f53ba
Yuhui Huang <mrhuangyuhui@gmail.com>
2022-01-31 08:45:57 +0000

    上传代码

---
+++ 09/9.2/p95-1.lua
@@ -0,0 +1,9 @@
+Lib = {}
+Lib.foo = function(x, y)
+    return x + y
+end
+Lib.goo = function(x, y)
+    return x - y
+end
+
+print(Lib.foo(2, 3), Lib.goo(2, 3)) --> 5       -1
---
+++ 09/9.2/p95-3.lua
@@ -0,0 +1,9 @@
+Lib = {}
+function Lib.foo(x, y)
+    return x + y
+end
+function Lib.goo(x, y)
+    return x - y
+end
+
+print(Lib.foo(2, 3), Lib.goo(2, 3)) --> 5       -1
